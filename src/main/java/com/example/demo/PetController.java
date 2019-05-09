e94dc8b474e49505fa9dc666f123ef95fecb174d
Nina Johnson <9makesthings@gmail.com>
2019-05-09 15:00:40 -0500

    working on POST method for pets

--- src/main/java/com/example/demo/PetController.java
+++ src/main/java/com/example/demo/PetController.java
@@ -5,7 +5,11 @@ import java.util.List;
 // import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 // import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +25,12 @@ public class PetController {
     @RequestMapping( value = "/api/pet", method = RequestMethod.GET )
     public List<Pet> list(){
         return jdbcTemplate.query( "SELECT * FROM pets", new PetRowMapper());
+    }
+
+    @PostMapping( value = "/api/pet", method = RequestMethod.POST )
+    public ResponseEntity <Pet> create(@RequestBody Pet pet ){
+        jdbcTemplate.query( "INSERT INTO pets (p_name, p_breed, p_color) VALUES (?, ?, ?)", new Object[] <Pet>);
+        return new ResponseEntity<Pet>(HttpStatus.OK);
     }
 
     // @RequestMapping( value = "/api/plants/{id}", method = RequestMethod.GET )
